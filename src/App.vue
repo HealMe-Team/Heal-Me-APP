6d6768be328c3998b8a65103bda2a340e277c250
DiegoAlonso04 <132526677+DiegoAlonso04@users.noreply.github.com>
2024-09-25 21:53:23 -0500

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,6 @@
     <Header />
     <Toolbar />
     <router-view /> <!-- Aquí se mostrarán los componentes de las rutas -->
-    <Main />
     <Footer />
 
   </div>
@@ -13,14 +12,12 @@
 <script>
 import Header from './components/Header/Header.component.vue';
 import Toolbar from './components/Toolbar/Toolbar.component.vue';
-import Main from './layouts/MainLayout.component.vue';
 import Footer from './components/Footer/Footer.component.vue';
 
 export default {
   components: {
     Header,
     Toolbar,
-    Main,
     Footer
   }
 }
